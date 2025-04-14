61d25708e2e7090d3481ed5980c4742380ecaf23
sharmajii7 <dishitsharma3741@gmail.com>
2025-04-15 04:45:57 +0530

    Updated directory structure

--- pipelines/ccfraud/submit.py
+++ pipelines/ccfraud/submit.py
@@ -81,7 +81,7 @@ YAML_CONFIG = OmegaConf.load(args.config)
 
 # path to the components
 COMPONENTS_FOLDER = os.path.join(
-    os.path.dirname(__file__), "..", "..", "components", "CCFRAUD"
+    os.path.dirname(__file__), "..", "..", "components"
 )
 
 ###########################
--- pipelines/upload_data/submit.py
+++ pipelines/upload_data/submit.py
@@ -41,7 +41,6 @@ COMPONENTS_FOLDER = os.path.join(
     "..",
     "..",
     "components",
-    "CCFRAUD",
 )
 
 
